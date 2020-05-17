8bbf4aac4f0d01a6d2cf6e8eade4292f52de5962
YueZhaoDreams <yuezhao@email.arizona.edu>
2020-05-17 17:07:03 -0400

    add code using jsoup

--- build.sbt
+++ build.sbt
@@ -3,3 +3,5 @@ name := "EvernoteStats"
 version := "0.1"
 
 scalaVersion := "2.12.6"
+
+libraryDependencies += "org.jsoup" % "jsoup" % "1.13.1"
--- src/main/scala/DataGenerator.scala
+++ src/main/scala/DataGenerator.scala
@@ -1,5 +1,9 @@
+import org.jsoup.Jsoup
+
 object DataGenerator {
   def main(args: Array[String]): Unit = {
     println("hello")
+    val doc = Jsoup.connect("http://example.com").get
+    doc.select("p").forEach(println)
   }
 }
