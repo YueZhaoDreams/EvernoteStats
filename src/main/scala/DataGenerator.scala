f87cf490f04ecbe2078506df85a4e113abe2847d
YueZhaoDreams <yuezhao@email.arizona.edu>
2020-05-17 22:52:06 -0400

    notes cleaning

--- src/main/scala/DataGenerator.scala
+++ src/main/scala/DataGenerator.scala
@@ -3,27 +3,40 @@ import java.util
 
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Element
+import collection.JavaConverters._
 
 object DataGenerator {
   def main(args: Array[String]): Unit = {
     val doc = Jsoup.parse(new File("./src/main/resources/notes/dreamnotes.html"), null)
-    val elements = doc.select("h1")
     val notes = new util.ArrayList[Note]
-    elements.forEach(element => addToNotes(element, notes))
+    val body = doc.select("body").html()
+    val x = body.split("<hr>").length
+    body.split("<hr>").foreach(
+      s => addToNotes(s, notes))
 
-//    println(notes)
+    //    println(notes)
   }
 
-  def addToNotes(value: Element, target: util.List[Note]): Unit = {
-    val note = new Note()
-    val name = value.html()
+  def addToNotes(value: String, target: util.List[Note]): Unit = {
+    val noteHtml = Jsoup.parse(value)
 
-    if (!name.matches("[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9].*")) {
+    val name = noteHtml.select("h1").html().trim
+    val trs = noteHtml.select("tr")
+    val note = new Note()
+    val tags = if (trs.size() >= 3) {
+      val tagArray: Array[String] = noteHtml.select("tr").get(2).select("i").html().split(",").map(s => s.trim)
+      new util.HashSet[String](util.Arrays.asList[String](tagArray: _*))
+    } else new util.HashSet[String]()
+
+    val content = noteHtml.select("span").html()
+
+    if (
+      !tags.contains("清醒梦") && (name.contains("清醒梦") || content.contains("清醒梦") || content.contains("清明梦"))) {
       println(name)
     }
 
-//    note.setName(name)
-//    note.setCreatedDate(name.substring(0, 7))
+    //    note.setName(name)
+    //    note.setCreatedDate(name.substring(0, 7))
 
     target.add(note)
   }
