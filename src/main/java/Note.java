44cd8cac5d2c7f6f4fbe85d052f80ce35096b8b7
YueZhaoDreams <yuezhao@email.arizona.edu>
2020-05-17 20:22:11 -0400

    add code using jsoup

---
+++ src/main/java/Note.java
@@ -0,0 +1,31 @@
+import java.util.Set;
+
+public class Note {
+    private String name;
+    private String createdDate;
+    private Set<Tag> tags;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(String createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public Set<Tag> getTags() {
+        return tags;
+    }
+
+    public void setTags(Set<Tag> tags) {
+        this.tags = tags;
+    }
+}
--- src/main/scala/DataGenerator.scala
+++ src/main/scala/DataGenerator.scala
@@ -1,9 +1,30 @@
+import java.io.File
+import java.util
+
 import org.jsoup.Jsoup
+import org.jsoup.nodes.Element
 
 object DataGenerator {
   def main(args: Array[String]): Unit = {
-    println("hello")
-    val doc = Jsoup.connect("http://example.com").get
-    doc.select("p").forEach(println)
+    val doc = Jsoup.parse(new File("./src/main/resources/notes/dreamnotes.html"), null)
+    val elements = doc.select("h1")
+    val notes = new util.ArrayList[Note]
+    elements.forEach(element => addToNotes(element, notes))
+
+//    println(notes)
+  }
+
+  def addToNotes(value: Element, target: util.List[Note]): Unit = {
+    val note = new Note()
+    val name = value.html()
+
+    if (!name.matches("[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9].*")) {
+      println(name)
+    }
+
+//    note.setName(name)
+//    note.setCreatedDate(name.substring(0, 7))
+
+    target.add(note)
   }
 }
